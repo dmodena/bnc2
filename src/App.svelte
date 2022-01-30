a042a7db7589c5fa1f7b8d17584c96e2e56f6ab4
Douglas Modena <d.modena@live.com>
2022-01-30 11:11:51 -0300

    Add GameHeader

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,11 @@
 <script>
+    import GameHeader from './game/GameHeader.svelte';
     import History from './game/History.svelte';
     import GuessView from './game/GuessView.svelte';
 </script>
 
 <div class="container">
+    <GameHeader />
     <History />
     <GuessView />
 </div>
---
+++ src/game/GameHeader.svelte
@@ -0,0 +1,4 @@
+<div class="row mt-3">
+    <h2 class="text-center">Bulls and Cows</h2>
+    <p class="text-center"><i class="bi bi-plus-circle"></i> Bull . <i class="bi bi-dash-circle"></i> Cow . <i class="bi bi-dash-lg"></i> Nothing</p>
+</div>
