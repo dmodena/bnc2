3a2849dcac99a49be8e2938cc45803fff7bf2276
Douglas Modena <d.modena@live.com>
2022-01-30 11:14:01 -0300

    Add Status

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,12 @@
     import GameHeader from './game/GameHeader.svelte';
     import History from './game/History.svelte';
     import GuessView from './game/GuessView.svelte';
+    import Status from './game/Status.svelte';
 </script>
 
 <div class="container">
     <GameHeader />
     <History />
     <GuessView />
+    <Status />
 </div>
---
+++ src/game/Status.svelte
@@ -0,0 +1,6 @@
+<div class="row mt-3">
+    <div class="col-12">
+    <label for="statusInput">Status</label>
+    <input type="text" class="form-control" id="statusInput" disabled>
+    </div>
+</div>
