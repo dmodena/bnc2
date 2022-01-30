8a7fd969a72a01ea626f844e3d394c6968b8e54d
Douglas Modena <d.modena@live.com>
2022-01-30 11:11:02 -0300

    Add History

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,9 @@
 <script>
+    import History from './game/History.svelte';
     import GuessView from './game/GuessView.svelte';
 </script>
 
 <div class="container">
+    <History />
     <GuessView />
 </div>
---
+++ src/game/History.svelte
@@ -0,0 +1,17 @@
+<style>
+    .history-container {
+        height: 160px;
+        overflow-y: scroll;
+    }
+</style>
+
+<div class="row">
+    <div class="col-12">
+    <div class="card">
+        <div class="card-header">History</div>
+        <div class="history-container">
+            <ul id="historyList" class="list-group list-group-flush"></ul>
+        </div>
+    </div>
+    </div>
+</div>
